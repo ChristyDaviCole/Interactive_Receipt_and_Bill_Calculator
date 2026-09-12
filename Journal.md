c044bcba50d1e59a5c381e48bba471ff38e823ca
ChristyDaviCole <christydavicole@gmail.com>
2026-09-12 12:59:04 -0500

    Phase 2: Interactive Item Collection

--- Journal.md
+++ Journal.md
@@ -1,2 +1,5 @@
 Journal Prompt 1: Instead of putting all the store details into a single storeInfo object, you could have saved each detail in its own standalone global variable. What are the advantages of grouping related data into an object versus managing separate variables?
 You can access all of the information about the store in one place with one object rather than having to reference several different variables. It's like keeping one file on the object rather than multiple ones.
+
+Journal Prompt 2: What happens if you set a value to a property of an object that didn’t exist before? How does that effect the ways that you can create and set object properties?
+JS will let you add the property even if it wasn't there before, which is useful because objects can start off empty and be built as the programs goes along.
--- script.js
+++ script.js
@@ -6,13 +6,46 @@ let storeInfo = {};
 
 //Prompt the user for the store’s name, the tax rate (as a percentage, e.g., 8 for 8%), and a standard table/service fee applied to every bill.
 //Store these values inside storeInfo with the properties name, taxRate, and tableFee.
-
 storeInfo.name = prompt("Store name? ");
 storeInfo.taxRate = prompt("Tax rate? ");
 storeInfo.tableFee = prompt("Table fee? ");
 
 //Log a welcome message to the console that prints the store name and the standard table fee.
-
 console.log("Welcome to ", storeInfo.name, " The standard table fee is ", storeInfo.tableFee);
 
 //Journal Prompt 1
+
+//Phase 2: Interactive Item Collection
+//Create an empty array named receiptItems. 
+//You will use this array to dynamically store food items as they are ordered.
+let receiptItems = [];
+
+//Use a while loop combined with prompt() to continuously ask the user if they want to enter a new item
+// (e.g., Y to continue, anything else to stop).
+let continueOrder = prompt("Would you like to add another item? (y to continue)");
+
+while (continueOrder === "y") {
+    //Inside the loop, prompt for the item’s name and its price.
+    let itemName = prompt("Name of item? ");
+    let priceOf = prompt("Price of item? ");
+
+    //Convert the price input to a number using Number().
+    let itemPrice = Number(priceOf);
+
+    //Package the name and price into an item object (e.g., { name: "Burger", price: 10.50 }).
+    let itemInfo = {};
+    itemInfo.name = itemName;
+    itemInfo.price = itemPrice;
+
+    //Add it to receiptItems using the .push() method.
+    receiptItems.push(itemInfo);
+
+    continueOrder = prompt("Would you like to add another item? (y to continue)");
+
+}
+
+//Once the loop ends, log the total count of items added and print the receiptItems array to the console to verify your data.
+
+console.log("Number of items:", receiptItems.length);
+
+//Journal Prompt 2
